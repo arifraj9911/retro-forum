234ae9efc8b19fb2d86bd658d0660766db3e19b6
arifraj199 <karif9514@gmail.com>
2024-03-03 01:40:22 +0600

    isActive functionality added

--- js/index.js
+++ js/index.js
@@ -23,11 +23,14 @@ const displayAllPost = (posts) => {
       comment_count,
       view_count,
       posted_time,
+      isActive,
     } = post;
-    card.innerHTML = `
+    if (isActive) {
+      card.innerHTML = `
         <div class="flex  bg-[#797DFC1A] p-10 rounded-3xl  gap-6 ">
-        <div>
-            <img class="w-16 h-16 rounded-2xl" src="${image}" alt="">
+        <div class = "relative">
+            <img class="absolute right-[-6px] top-[-12px]" src="assets/images/dot_green.svg" >
+            <img class="w-16 h-16 rounded-xl" src="${image}" alt="">
         </div>
         <div>
             <div class="flex gap-8">
@@ -62,6 +65,48 @@ const displayAllPost = (posts) => {
         </div>
     </div>
         `;
+    } else {
+      card.innerHTML = `
+        <div class="flex  bg-[#797DFC1A] p-10 rounded-3xl  gap-6 ">
+        <div class = "relative">
+            <img class="absolute right-[-6px] top-[-12px]" src="assets/images/dot_red.svg" >
+            <img class="w-16 h-16 rounded-xl" src="${image}" alt="">
+        </div>
+        <div>
+            <div class="flex gap-8">
+                <span>#${category}</span>
+                <span>Author: ${author.name}</span>
+            </div>
+            <h3 class="text-xl font-bold mt-2 mb-4">${title}</h3>
+            <p class="w-5/6">${description}</p>
+            <hr class="my-5">
+            <div class="flex justify-between">
+                <div class="flex gap-7 items-center">
+                    <div class="flex justify-center items-center gap-2">
+                        <span><img src="assets/images/message2.svg" alt=""></span>
+                        <span>${comment_count}</span>
+                    </div>
+                    <div class="flex justify-center items-center gap-2">
+                        <span><img src="assets/images/tabler-icon-eye.svg" alt=""></span>
+                        <span>${view_count}</span>
+                    </div>
+                    <div class="flex justify-center items-center gap-2">
+                        <span><img src="assets/images/time.svg" alt=""></span>
+                        <span>${posted_time}</span>
+                    </div>
+                </div>
+                <div onclick="cardMark('${title.replace(
+                  "'",
+                  ""
+                )}','${view_count}')">
+                    <img src="assets/images/message.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+        `;
+    }
+
     cardContainer.appendChild(card);
   });
 };
